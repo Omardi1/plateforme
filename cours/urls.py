dcf05013511a288bb22e92f4a9c3ef2f6e5abe36
Omar Diombaty <omar.diombaty@example.com>
2025-03-26 14:54:55 +0100

    Ajout des permissions et authentification JWT dans urls.py

--- cours/urls.py
+++ cours/urls.py
@@ -7,6 +7,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 router = DefaultRouter()
 router.register(r'categories', views.CategoryViewSet)
@@ -27,3 +33,21 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def get_tokens_for_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    
+    user = authenticate(username=username, password=password)
+    
+    if user is not None:
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        })
+    
+    return Response({'error': 'Invalid Credentials'}, status=400)
